0f99138867dc61ef7397500ba61bad997619b371
ꜱᴜᴋʜ <123292087+Badmunda50@users.noreply.github.com>
2025-02-01 12:19:46 +0530

    Update calls.py

--- Music/core/calls.py
+++ Music/core/calls.py
@@ -8,10 +8,10 @@ from pyrogram.errors import (
     UserNotParticipant,
 )
 from pyrogram.types import InlineKeyboardMarkup
-from pytgcalls import PyTgCalls, StreamType
-from pytgcalls.exceptions import AlreadyJoinedError, NoActiveGroupCall
-from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import MediumQualityAudio, MediumQualityVideo
+from ntgcalls import PyTgCalls, StreamType
+from ntgcalls.exceptions import AlreadyJoinedError, NoActiveGroupCall
+from ntgcalls.types.input_stream import AudioPiped, AudioVideoPiped
+from ntgcalls.types.input_stream.quality import MediumQualityAudio, MediumQualityVideo
 
 from config import Config
 from Music.helpers.buttons import Buttons
